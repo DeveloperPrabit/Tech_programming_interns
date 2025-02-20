f929636d4bb4c662df456476972c9cd842c19eb1
Nabin2060 <141759067+Nabin2060@users.noreply.github.com>
2025-02-20 14:29:29 +0545

    day1

--- backend/index.js
+++ backend/index.js
@@ -1,29 +1,35 @@
-// import express from "express";
-// // import cors from "cors";
-// // import dotenv from "dotenv";
-//  import dotenv from "dotenv";
-
-// dotenv.config();
-
-// const app = express();
-
-// app.use(express.json());
-
-
-
-// // Server
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
 
 const express = require('express')
+const dotenv =require('dotenv');
+const router = express.Router();
+
+dotenv.config();
 const app = express()
-const port = 3000
+const PORT =process.env.PORT || 7000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World ')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(PORT, () => {
+  console.log(`Application is running on port ${PORT}`)
+})
+
+//register scenario
+router.post('/register',(req,res)=>{
+  res.json({msg:"movies added"});
+  
+})
+//login scenario
+router.post('/login',(req,res)=>{
+  try{
+  const {email,password}=req.body;
+  console.log({email,password});
+  
+  res.json({msg:"movies added"});
+  }catch(e){
+    console.log(e);
+    
+  }
+  
 })
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+// import Home from '../pages/Home'
+import ErrorPage from './pages/ErrorPage'
+import AdminSidebar from './layouts/adminLayouts/AdminSidebar'
+import Profile from './pages/Profile'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
    <>
-   <div className='flex justify-items-center text-30 bg-purple-500'>Welcome News Portal App :)</div>
+   <div className='flex justify-items-center text-30 bg-purple-500'>Welcome News Portal App :) 
+   </div>
+   {/* <ErrorPage /> */}
+   <Header />
+   <Footer />
+   {/* <AdminSidebar /> */}
+   {/* <Profile /> */}
    </>
   )
 }
